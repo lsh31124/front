c99b35fd68dadc20eeb34e7458088eda199ea8e7
Peter-J-Sung <ivsinji746@naver.com>
2022-12-05 13:20:57 +0900

    Issue 1/crasetup (#2)
    * issue-1: cra pply
    
    * removing node_modules
    
    * issue-1: eslint append and prettier apply

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+}
