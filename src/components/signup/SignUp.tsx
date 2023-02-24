597f26d028800ec37d4234fc2d29c6a9393ccf96
la1av1a <zldqkrktqk@gmail.com>
2023-02-24 22:12:10 +0900

    mount 애니메이션 추가

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -7,9 +7,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import React, { useRef, useState } from 'react'
 import Pages from './pages'
+import styles from './SignUp.module.css'
 import UserId, { UserIdRef } from './UserId'
 import UserName, { InputRefs } from './UserName'
-
 const theme = createTheme()
 
 export default function SignUp(): React.ReactElement {
@@ -35,11 +35,14 @@ export default function SignUp(): React.ReactElement {
           <Typography component="h1" variant="h5"></Typography>
           Sign Up
           <React.Fragment>
-            {page == Pages.userId && <UserId setPage={setPage} ref={userIdRef}></UserId>}
-            {page === Pages.userName && <UserName setPage={setPage} ref={userNameRef}></UserName>}
+            <Box className={styles.mount1}>
+              {page == Pages.userId && <UserId setPage={setPage} ref={userIdRef}></UserId>}
+              {page === Pages.userName && <UserName setPage={setPage} ref={userNameRef}></UserName>}
+            </Box>
           </React.Fragment>
         </Box>
       </Container>
     </ThemeProvider>
   )
 }
+declare module '*.module.css'
